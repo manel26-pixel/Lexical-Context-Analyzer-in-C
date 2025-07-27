c3200ab70f714166acbbd38ab43de11448a4d290
Manel <ferchichimanelma@gmail.com>
2025-07-27 21:47:32 +0100

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -1,66 +1,82 @@
-#ifndef fonction_h
-#define fonction_h
+#ifndef FONCTION_H
+#define FONCTION_H
+
+// Standard Libraries
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
 #include <time.h>
-#include "FONCTION.c"
-#define max 256
 
-/////////////////////////////////////////////////definition des structs//////////////////////////////////////////
-//1 DEFINIR LE TYPE LISTE
-typedef struct noeud*liste;
-typedef struct noeud{
-    char*word;
+#define MAX 256
+
+/////////////////////////////////////////////////
+//               STRUCT DEFINITIONS            //
+/////////////////////////////////////////////////
+
+// 1. Linked list for storing words
+typedef struct noeud* liste;
+typedef struct noeud {
+    char *word;
     liste suivant;
-}noeud;
-// DEFINIR LISTE FREQ 
-typedef struct noeuds* listefreq;
-typedef struct noeuds{
-    int nbapp;
-    char*mot;
-    listefreq suivants;
-}noeuds;
-//3 DEFINIR LE TYPE DICTIONNAIRE 
-typedef struct dict*dictionnaire;
-typedef struct dict{
-  char* motDict;
-  listefreq contextes;
-  dictionnaire suivantDict;
-}dict;
+} noeud;
 
-///////////////////////////////////////////les fonction/////////////////////////////////////////////////////////////
-//reponse1
-//fonction pour allouer une chaine de caractère
-char * allouerChaine(int n);
-long taille(FILE*f );
-char*lire (FILE*f);
-liste creernoeud(char*mot);
+// 2. Frequency list (word + count)
+typedef struct noeuds* listefreq;
+typedef struct noeuds {
+    int nbapp;
+    char *mot;
+    listefreq suivants;
+} noeuds;
+
+// 3. Dictionary (word + frequency list of contexts)
+typedef struct dict* dictionnaire;
+typedef struct dict {
+    char *motDict;
+    listefreq contextes;
+    dictionnaire suivantDict;
+} dict;
+
+/////////////////////////////////////////////////
+//             FUNCTION DECLARATIONS           //
+/////////////////////////////////////////////////
+
+// ----------- Utility & File Operations ----------
+char *allouerChaine(int n);
+long taille(FILE *f);
+char *lire(FILE *f);
+
+// ----------- Word List (liste) Operations ----------
+liste creernoeud(char *mot);
 void afficherListe(liste L);
 liste creeListeChaine(char *ch);
 void supprimerdoublants(liste L);
-int count( liste head, char * search);
+int count(liste head, char *search);
 liste duplicate(liste l);
-void permuter(listefreq L1,listefreq L2);
+int nombremotliste(liste L);
+
+// ----------- Frequency List (listefreq) ----------
+listefreq creernoeudlistefrequence(char *word, int nombreocc);
+void afficherlistefrequence(listefreq list);
+void permuter(listefreq L1, listefreq L2);
 void selectionSortAscending(listefreq tete);
 void selectionSortdescending(listefreq tete);
-listefreq creernoeudlistefrequence(char*word,int nombreocc);
-void afficherlistefrequence(listefreq list);
-liste contexterMot(liste list, char* m);
-listefreq  frequencesDe(liste ContexteAvcDouble);
-void affichagedictionnaire(dictionnaire Ldiction);
-dictionnaire creerNouedDictionnaire(char* mot, listefreq contextesMot);
+
+// ----------- Context & Frequency Analysis ----------
+liste contexterMot(liste list, char *m);
+listefreq frequencesDe(liste ContexteAvcDouble);
+
+// ----------- Dictionary (dictionnaire) ----------
+dictionnaire creerNouedDictionnaire(char *mot, listefreq contextesMot);
 dictionnaire creationdictionary(liste desmots);
-char*maxcontexte(dictionnaire dict,char*m);
-int freqCont(dictionnaire dict ,char*m , char*n);
-int nombremotliste(liste L);
+void affichagedictionnaire(dictionnaire Ldiction);
+char *maxcontexte(dictionnaire dict, char *m);
+int freqCont(dictionnaire dict, char *m, char *n);
+
+// ----------- Random Generation ----------
 int generernbr(int n);
-char*aleatoirement(int k,dictionnaire d);
-void genererTEXTE(dictionnaire d,int naff,char*aword);
+char *aleatoirement(int k, dictionnaire d);
+void genererTEXTE(dictionnaire d, int naff, char *aword);
 
-
-#endif
-
-
+#endif // FONCTION_H
